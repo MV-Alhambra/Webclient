4f2e265d9f9fa5bceedfe35eda5a6b81017ca8e0
Maarten Vercruysse <maarten.vercruysse@student.howest.be>
2020-03-18 22:50:24 +0100

    fix validate sonar code smells

--- src/assets/js/communication.js
+++ src/assets/js/communication.js
@@ -1,9 +1,9 @@
 "use strict";
 
-function fetchFromServer(url, httpVerb, requestBody){
-    let options= {
+function fetchFromServer(url, httpVerb, requestBody) {
+    let options = {
         method: httpVerb,
-        headers:  {}
+        headers: {}
     };
     options.headers["Content-Type"] = "application/json";
 
@@ -12,29 +12,10 @@ function fetchFromServer(url, httpVerb, requestBody){
 
     return fetch(url, options)
         .then((response) => {
-        if (!response.ok) {
-            console.error('%c%s','background-color: red;color: white','! An error occurred while calling the API');
-            console.table(response);
-        }
-        return response.json();
-    })
-    .then((jsonresponseyouarelookingfor) => {
-        return jsonresponseyouarelookingfor
-    })
-}
-
-function fetchFromServerWithReturnError(url, httpVerb, requestBody){
-    let options= {};
-    options.method = httpVerb;
-
-    options.headers = {};
-    options.headers["Content-Type"] = "application/json";
-
-    // Don't forget to add data to the body when needed
-    options.body = JSON.stringify(requestBody);
-
-    return fetch(url, options)
-        .then((response) => {
+            if (!response.ok) {
+                console.error('%c%s', 'background-color: red;color: white', '! An error occurred while calling the API');
+                console.table(response);
+            }
             return response.json();
         })
         .then((jsonresponseyouarelookingfor) => {
@@ -42,11 +23,11 @@ function fetchFromServerWithReturnError(url, httpVerb, requestBody){
         })
 }
 
-function fetchFromServerWithReturnErrorAndJson(url, httpVerb, requestBody){
-    let options= {};
-    options.method = httpVerb;
-
-    options.headers = {};
+function fetchFromServerWithReturnErrorAndJson(url, httpVerb, requestBody) {
+    let options = {
+        method: httpVerb,
+        headers: {}
+    };
     options.headers["Content-Type"] = "application/json";
 
     // Don't forget to add data to the body when needed
