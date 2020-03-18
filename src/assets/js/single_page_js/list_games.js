ae5ad572a45374a2c85aec9c074d649add419697
Maarten Vercruysse <maarten.vercruysse@student.howest.be>
2020-03-19 00:14:31 +0100

    Add functionality link on list games

--- src/assets/js/single_page_js/list_games.js
+++ src/assets/js/single_page_js/list_games.js
@@ -1,18 +1,27 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded', init);
 
-function init(){
+function init() {
     fillLobbyList();
-    document.querySelector('main>a').addEventListener('click',fillLobbyList);
+    document.querySelector('main>a').addEventListener('click', fillLobbyList)
 }
 
 function fillLobbyList() {
     const list = document.querySelector('main ul');
-    list.innerHTML='';
-    returnGames().then(function (result) {
-        result.forEach( lobby => {
-            list.innerHTML +=  `<li><p>${lobby.id}</p> <em>${lobby.playerCount}/6 players</em> <a href="join_game.html">Join game</a></li>`;
-        });
+    list.innerHTML = '';
+    returnGames().then(result => {
+        result.forEach(lobby => {
+            list.innerHTML += `<li><p>${lobby.id}</p> <em>${lobby.playerCount}/6 players</em> <a data-id='${lobby.id}'>Join game</a></li>`;
+        })
     });
+    document.querySelectorAll('main ul li').forEach(li => li.addEventListener('click', activateJoin));
+
+}
+
+function activateJoin(e) {
+    e.preventDefault();
+    const ID = e.target.getAttribute('data-id');
+    localStorage.setItem('gameId',ID);
+    window.location.replace('./join_game.html');
 }
