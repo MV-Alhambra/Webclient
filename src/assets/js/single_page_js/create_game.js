aaa8792f98e586eef95c7c4b0657449f0e293171
Maarten Vercruysse <maarten.vercruysse@student.howest.be>
2020-03-18 22:21:15 +0100

    add localstorage for create game

--- src/assets/js/single_page_js/create_game.js
+++ src/assets/js/single_page_js/create_game.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 let gameId;
 let playerToken;
+let gameName;
 
 function init() {
     document.querySelector('form').addEventListener('submit', createGame)
@@ -13,6 +14,7 @@ function createGame(e) {
     e.preventDefault();
 
     const name = document.querySelector('#Player_Name').value;
+    gameName = document.querySelector('#Game_Name').value;
 
     if (!gameId) {
         addGame().then(id => {
@@ -22,14 +24,15 @@ function createGame(e) {
     } else {
         addLocalPlayer(e, name)
     }
-
-
 }
 
 function addLocalPlayer(e, name) {
     addPlayer(gameId, name.toLowerCase()).then(response => {
         if (response.ok) {
             playerToken = response.json;
+            localStorage.setItem('gameId', gameId);
+            localStorage.setItem('playerToken', playerToken);
+            localStorage.setItem('gameName',gameName);
             window.location.replace('./temp.html');
         } else {
             document.querySelector('.error').innerHTML = 'That player name is not allowed!';
