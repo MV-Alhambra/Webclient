483529c15a92819947e018493ee1d844bafbe365
maxim <maxim.decherf@student.howest.be>
2020-03-19 14:21:36 +0100

    Yes or No in confirmation popup

--- src/assets/js/single_page_js/leave_game.js
+++ src/assets/js/single_page_js/leave_game.js
@@ -3,11 +3,18 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-   document.querySelector('.leavePopup').addEventListener('click', confirmLeaving)
+   document.querySelector('.leavePopup').addEventListener('click', confirmLeaving);
+   document.querySelector('#returnToGame').addEventListener('click', closePopup);
 }
 
 function confirmLeaving(e) {
     e.preventDefault();
     let popup = document.querySelector('.hidden');
     popup.style.display = "inline";
+}
+
+function closePopup(e) {
+    e.preventDefault();
+    let popup = document.querySelector('.hidden');
+    popup.style.display = "none";
 }
