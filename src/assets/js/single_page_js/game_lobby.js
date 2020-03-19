e290c0dd0535f081a80a74a8c059399de87ac76b
Maarten Vercruysse <maarten.vercruysse@student.howest.be>
2020-03-20 00:39:51 +0100

    Fix copy invite link button

--- src/assets/js/single_page_js/game_lobby.js
+++ src/assets/js/single_page_js/game_lobby.js
@@ -3,18 +3,27 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    document.querySelector('#copy').addEventListener('click', copyToClipboard);
+    document.querySelector('#copy').addEventListener('click', copy);
 }
 
-function copyToClipboard() {
-    let copyURL = document.querySelector('#inviteURL');
-    let range = document.createRange();
-    range.selectNode(copyURL);
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
-    document.querySelector('#copy').addEventListener('click', copyToClipboard);
-
+function copy() {
+    let copyText = document.querySelector("#inviteURL");
+    selectText(copyText);
+    document.execCommand("copy");
 }
 
+function selectText(text) {
+
+    if (document.body.createTextRange) { // ms
+        let range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { // moz, opera, webkit
+        let selection = window.getSelection();
+        let range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
 
