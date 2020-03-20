d978e8d3c50f88042a4af98b875f3ac37e4045bc
mitch <mitch.rollez@student.howest.be>
2020-03-20 14:14:13 +0100

    add popup

--- src/assets/js/single_page_js/game.js
+++ src/assets/js/single_page_js/game.js
@@ -1,2 +1,12 @@
 "use strict";
 
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    document.querySelector('#pspopup').addEventListener('click', showpointsystem);
+}
+
+function showpointsystem(){
+    document.querySelector('.pointsystem').style.display = 'flex';
+}
+
