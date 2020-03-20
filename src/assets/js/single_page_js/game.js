684a6005331173d587536cf4a0d9fbfeeef26880
mitch <mitch.rollez@student.howest.be>
2020-03-20 14:28:11 +0100

    reposition pointsystem button

--- src/assets/js/single_page_js/game.js
+++ src/assets/js/single_page_js/game.js
@@ -4,9 +4,14 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init(){
     document.querySelector('#pspopup').addEventListener('click', showpointsystem);
+    document.querySelector('.close').addEventListener('click', closepointsystem);
 }
 
 function showpointsystem(){
     document.querySelector('.pointsystem').style.display = 'flex';
+}
+
+function closepointsystem(){
+    document.querySelector('.pointsystem').style.display = 'none';
 }
 
