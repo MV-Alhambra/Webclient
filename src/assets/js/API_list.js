6adce58ee74c666c0e651e7668fb961c5f3f8611
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-04-05 23:57:52 +0200

    Refactor of the fetch functions and API functions

--- src/assets/js/API_list.js
+++ src/assets/js/API_list.js
@@ -1,43 +1,37 @@
 'use strict';
 
 function returnGames() {   // Show all games from your group lobby
-    return fetchFromServer(`${config.root}games?details=true&prefix=group${config.groupnumber}`, 'GET').then(function (response) {
-        return response;  // These are the games in your group lobby, the first time you'll call this, this will be an empty array
-    });
+    return fetchJSON(`${config.root}games?details=true&prefix=group${config.groupnumber}`, 'GET');
 }
 
 function addGame() {   // Add a new game in your lobby
-    return fetchFromServer(`${config.root}games`, 'POST', {prefix: `group${config.groupnumber}`}).then(function (response) {
-        return response; // The reply here is the game ID, keep it secret, keep it safe!
-    });
+    return fetchJSON(`${config.root}games`, 'POST', '', {prefix: `group${config.groupnumber}`});
 }
 
 function addPlayer(gameId, playerName) { //add player to your lobby, player name needs to be lowercase
-    return fetchFromServerWithReturnErrorAndJson(`${config.root}games/${gameId}/players`, 'POST', {playerName: playerName}).then(function (response) {
-        return response;
-    });
+    return fetchRaw(`${config.root}games/${gameId}/players`, 'POST', '', {playerName: playerName});
 }
 
 function getGame(gameId, token) { // Get the state of a game
-    return fetchWithToken(`${config.root}games/${gameId}`, 'GET', token).then(response => response);
+    return fetchJSON(`${config.root}games/${gameId}`, 'GET', token);
+}
+
+function setPlayerReady(gameId, token, playerName) {
+    return fetchJSON(`${config.root}games/${gameId}/players/${playerName}/ready`, "PUT", token);
+}
+
+function setPlayerUnready(gameId, token, playerName) {
+    return fetchJSON(`${config.root}games/${gameId}/players/${playerName}/ready`, "DELETE", token);
+}
+
+async function leaveGame(gameId, token, playerName) {
+    return fetchJSON(`${config.root}games/${gameId}/players/${playerName}`, "DELETE", token);
 }
 
 function getGameProperty(gameId, token, property) {
     return getGame(gameId, token).then(response => {
         return response[property];
     });
-}
-
-function setPlayerReady(gameId, token, playerName) {
-   return  fetchWithToken(`${config.root}games/${gameId}/players/${playerName}/ready`, "PUT", token);
-}
-
-function setPlayerUnready(gameId, token, playerName) {
-    return fetchWithToken(`${config.root}games/${gameId}/players/${playerName}/ready`, "DELETE", token);
-}
-
-async function leaveGame(gameId, token, playerName) {
-    return fetchWithToken(`${config.root}games/${gameId}/players/${playerName}`, "DELETE", token);
 }
 
 async function getPlayerCount(gameId, token) {
@@ -57,6 +51,6 @@ async function getGamePlayers(gameId, token) {
 }
 
 async function getGameCurrentPlayer(gameId, token) {
-    return  getGameProperty(gameId, token, 'currentPlayer');
+    return getGameProperty(gameId, token, 'currentPlayer');
 }
 
