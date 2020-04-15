1cda16666765feaeac4161616e30d8b38dca7ddf
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-04-15 04:03:43 +0200

    Add js for moving a building to the reserve

--- src/assets/js/single_page_js/game-redesign.js
+++ src/assets/js/single_page_js/game-redesign.js
@@ -21,37 +21,55 @@ function addRedesignSelectors() {
         document.querySelectorAll("#map .building").forEach(building => building.addEventListener("drag", dragBuilding));
         document.querySelectorAll("#map .building").forEach(building => building.addEventListener("dragstart", dragBuildingStart));
         document.querySelectorAll("#map .building").forEach(building => building.addEventListener("dragend", dragBuildingEnd));
+        reserveWrapper.parentElement.addEventListener("drop", dragBuildingDrop); // this triggers when an item gets dropped in it
+        reserveWrapper.parentElement.addEventListener("dragover", ev => ev.preventDefault());//this sets the location where i can drop the item
     }
 }
 
 function dragBuilding(e) {
     buildingDrag.style.top = (e.clientY) + "px";
     buildingDrag.style.left = (e.clientX) + "px";
-    buildingDrag.classList.remove("hidden");
+
 }
 
 function dragBuildingStart(e) {
-    buildingDrag.innerHTML = createBuilding(convertBuildingToObject(e.target));
+    buildingDrag.innerHTML = e.target.outerHTML;
     e.target.classList.add("draggedBuilding");
-
+    buildingDrag.style.top = (e.clientY) + "px";
+    buildingDrag.style.left = (e.clientX) + "px";
+    buildingDrag.classList.remove("hidden");
 }
 
-function dragBuildingEnd(e) {
-    if (e.target.closest("#reserve")) {//do it if it finds the reserve
-        console.log("found");
-        const index = parseInt(buildingDrag.firstElementChild.getAttribute("data-index"));
-        console.log(convertIndexToStaticLocation(index));
-    } else {
-        console.log("not found");
-        buildingDrag.classList.add("hidden");
-        setMap();
-    }
+function dragBuildingEnd() {
+    buildingDrag.classList.add("hidden");
+    setMap();
+}
+
+function dragBuildingDrop(e) {
+    const index = parseInt(buildingDrag.firstElementChild.getAttribute("data-index"));
+    setCityBuildingToReserve(gameId,token,playerName,convertIndexToLocation(index)).then(response=>responseHandler(response,e));
 }
 
 function convertIndexToStaticLocation(index) {
     return {
         col: index % mapSize,
         row: parseInt(index / mapSize)
+    }
+}
+
+function convertStaticToDynamicLocation(staticLocation) {
+    const mapRadius = (mapSize - 1) / 2;
+    return {
+        row: staticLocation.row - mapRadius,
+        col: staticLocation.col - mapRadius
+    }
+}
+
+function convertIndexToLocation(index) {
+    const mapRadius = (mapSize - 1) / 2;
+    return {
+        col: (index % mapSize) - mapRadius,
+        row: (parseInt(index / mapSize)) - mapRadius
     }
 }
 
