40b6afc0fc0ebf43f2218037b751076628c2ec75
maxim <maxim.decherf@student.howest.be>
2020-05-12 15:40:49 +0200

    function to get all players from the game and put hem in a map

--- src/assets/js/single_page_js/end_game.js
+++ src/assets/js/single_page_js/end_game.js
@@ -1,2 +1,28 @@
 "use strict";
 
+const gameId = localStorage.getItem('gameId');
+const playerName = localStorage.getItem('playerName');
+
+const winOrLoseTitle = document.querySelector('h1');
+const playerNameContainer = document.querySelector('section h3');
+const score = document.querySelector('section h4');
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+function init() {
+
+}
+
+
+
+function getPlayersAndScore() { // loads the scoreboard in
+    getGamePlayers(gameId).then(players => {
+        let scoreboard = [];
+        players.forEach(player => {
+            scoreboard[player.name] = player.score;
+        });
+        return scoreboard
+    });
+}
