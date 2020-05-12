b451fdf477e9831059dbe0f0347fba25ddae5b07
maxim <maxim.decherf@student.howest.be>
2020-05-12 16:45:31 +0200

    load in the players and show there name and score

--- src/assets/js/single_page_js/end_game.js
+++ src/assets/js/single_page_js/end_game.js
@@ -3,29 +3,34 @@
 const gameId = localStorage.getItem('gameId');
 const playerName = localStorage.getItem('playerName');
 
-const winOrLoseTitle = document.querySelector('h1');
-const playerNameContainer = document.querySelector('section h3');
-const score = document.querySelector('section h4');
-
-
+const scoreBoard = document.querySelector('#players');
 
 const scoreBoardTest = [
     { name: 'jan', score: 200 },
     { name: 'maxim', score: 400 },
     { name: 'Joe', score: 300 },
-];
-
-
+]; // just for testing so i dont have to make a game over and over again
 
 document.addEventListener('DOMContentLoaded', init);
 
 
 function init() {
-    
-
+    loadInScore(getPlayersAndScore());
 }
 
-
+function loadInScore(playersScore) {
+    scoreBoard.innerHTML = "";
+    playersScore.forEach( (player, index) =>
+        scoreBoard.innerHTML += `<section>
+        <h2>${index + 1}</h2>
+        <h3>${player.name}</h3>
+        <h4>${player.score}</h4>
+        <h5>Title:</h5>
+        <h6>Description</h6>
+        <a href="#">Show village</a>
+    </section>`
+    )
+}
 
 function getPlayersAndScore() { // loads the scoreboard in
     getGamePlayers(gameId).then(players => { //todo add token later
