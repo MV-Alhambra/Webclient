7c432f1267f6f7850519df64dd23fc586987613d
maxim <maxim.decherf@student.howest.be>
2020-05-12 16:19:52 +0200

    order the players in the map by score

--- src/assets/js/single_page_js/end_game.js
+++ src/assets/js/single_page_js/end_game.js
@@ -8,21 +8,39 @@ const playerNameContainer = document.querySelector('section h3');
 const score = document.querySelector('section h4');
 
 
+
+const scoreBoardTest = [
+    { name: 'jan', score: 200 },
+    { name: 'maxim', score: 400 },
+    { name: 'Joe', score: 300 },
+];
+
+
+
 document.addEventListener('DOMContentLoaded', init);
 
 
 function init() {
+    
 
 }
 
 
 
 function getPlayersAndScore() { // loads the scoreboard in
-    getGamePlayers(gameId).then(players => {
+    getGamePlayers(gameId).then(players => { //todo add token later
         let scoreboard = [];
         players.forEach(player => {
-            scoreboard[player.name] = player.score;
+            scoreboard.push({name : player.name , score : player.score})
         });
-        return scoreboard
+        return orderByScore(scoreboard);
     });
 }
+
+function orderByScore(scoreboard) {
+    scoreboard.sort(function (a, b) {
+        return  b.score - a.score;
+    });
+    return scoreboard;
+}
+
