16c62e5d8337407b595d1368e0679223e6fd31dc
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2020-05-19 18:53:51 +0200

    Remove code smells reported by sonar

--- src/assets/js/single_page_js/end_game.js
+++ src/assets/js/single_page_js/end_game.js
@@ -4,26 +4,31 @@ const gameId = localStorage.getItem('gameId');
 const playerName = localStorage.getItem('playerName');
 const token = localStorage.getItem('playerToken');
 
-const scoreBoard = document.querySelector('#players');
-const winOrLose = document.querySelector('h1');
-
-const city = document.querySelector("#city");
-const cityMapTitle = document.querySelector("#cityMap h5");
-const cityMapWrapper = document.querySelector("#cityMap div div");
+let scoreBoard;
+let winOrLose;
+let cityMap;
+let cityMapTitle;
+let cityMapWrapper;
+let reserveWrapper;
 let cityZoomOut = null;
 let cityZoomIn = null;
-const reserveWrapper = document.querySelector("#reserve div");
 let mapSize = 3;
-let maxMapSize = 13;
+const maxMapSize = 13;
 let nameOfCity = null;
 
 document.addEventListener('DOMContentLoaded', init);
 
-
 function init() {
-    updateMapSize();
     window.addEventListener('resize', updateMapSize);
     document.querySelector("#city .close").addEventListener("click", closeCity);
+    scoreBoard = document.querySelector('#players');
+    winOrLose = document.querySelector('h1');
+    cityMap = document.querySelector("#city");
+    cityMapTitle = document.querySelector("#cityMap h5");
+    cityMapWrapper = document.querySelector("#cityMap div div");
+    reserveWrapper = document.querySelector("#reserve div");
+
+    updateMapSize();
     getPlayersAndScore().then(response => {
         loadInScore(response);
         checkWin(response);
@@ -31,7 +36,6 @@ function init() {
     });
 
 }
-
 
 function loadInScore(playersScore) {
     scoreBoard.innerHTML = "";
@@ -74,19 +78,18 @@ function checkWin(response) {
 }
 
 function showCity(e) {
-   nameOfCity = e.target.getAttribute("data-name");
+    nameOfCity = e.target.getAttribute("data-name");
     document.querySelector('.popup').classList.add('flex');
     cityMapTitle.innerHTML = `The town of <span> ${nameOfCity}</span>`;
     setCity();
 }
 
-
-function setCity(y) { // loads in the map
-    getGamePlayerProperty(gameId, token, nameOfCity, "city").then(cityMap => {
+function setCity() { // loads in the map
+    getGamePlayerProperty(gameId, token, nameOfCity, "city").then(city => {
         updateMapSize();
         cityMapWrapper.className = 'map' + mapSize;//set the size of the map
         cityMapWrapper.innerHTML = `<aside><p id="zoom_in_city">+</p><p id="zoom_out_city">-</p></aside>`;
-        convertCityToMap(cityMap).forEach(row => row.forEach(cell => cityMapWrapper.innerHTML += createBuilding(cell)));
+        convertCityToMap(city).forEach(row => row.forEach(cell => cityMapWrapper.innerHTML += createBuilding(cell)));
         cityZoomIn = document.querySelector("#zoom_in_city");
         cityZoomOut = document.querySelector("#zoom_out_city");
         cityZoomIn.addEventListener('click', () => zoomIn(cityZoomOut, setCity));
@@ -97,53 +100,6 @@ function setCity(y) { // loads in the map
 
 function updateMapSize() { //Makes the map square, so far only works when height is bigger than width
     cityMapWrapper.style.width = cityMapWrapper.clientHeight + "px";
-}
-
-function zoomButtonHider(btnZoomIn, btnZoomOut) { //logic for making the buttons invisible
-    if (mapSize === maxMapSize) {
-        btnZoomOut.classList.add("inactive");
-    } else if (mapSize === 3) {
-        btnZoomIn.classList.add("inactive");
-    } else {
-        btnZoomOut.classList.remove("inactive");
-        btnZoomIn.classList.remove("inactive");
-    }
-}
-
-
-function convertCityToMap(city) { //converts the city into the size of the map
-    const map = [...Array(mapSize)].map(() => Array(mapSize).fill(null)); //creates an empty/null 2 dimensional array
-    const cityCenter = (city.length + 1) / 2;
-    const mapCenter = (mapSize + 1) / 2;
-    const diffCenter = Math.abs(cityCenter - mapCenter);
-    if (cityCenter > mapCenter) {
-        for (let row = 0; row < mapSize; row++) {
-            for (let col = 0; col < mapSize; col++) {
-                map[row][col] = city[row + diffCenter][col + diffCenter];
-            }
-        }
-    } else if (cityCenter < mapCenter) {
-        const citySize = city.length;
-        for (let row = 0; row < citySize; row++) {
-            for (let col = 0; col < citySize; col++) {
-                map[row + diffCenter][col + diffCenter] = city[row][col];
-            }
-        }
-    } else {
-        return city;
-    }
-    return map;
-}
-
-function zoomIn(btnZoomOut, setFunction) { // changes the mapSize and holds logic for the buttons
-    if (mapSize !== 3) {
-        if (mapSize === maxMapSize) {
-            btnZoomOut.classList.remove("inactive");
-        }
-        mapSize -= 2;
-        localStorage.setItem("mapsize", mapSize);
-        setFunction();
-    }
 }
 
 function zoomOut(btnZoomIn, setFunction) {  // changes the mapSize and holds logic for the buttons
@@ -157,7 +113,53 @@ function zoomOut(btnZoomIn, setFunction) {  // changes the mapSize and holds log
     }
 }
 
-function createBuilding(building, index = -1, canBeDragged = false) { //receives an building object and turns it into html for a building
+function zoomIn(btnZoomOut, setFunction) { // changes the mapSize and holds logic for the buttons
+    if (mapSize !== 3) {
+        if (mapSize === maxMapSize) {
+            btnZoomOut.classList.remove("inactive");
+        }
+        mapSize -= 2;
+        localStorage.setItem("mapsize", mapSize);
+        setFunction();
+    }
+}
+
+function zoomButtonHider(btnZoomIn, btnZoomOut) { //logic for making the buttons invisible
+    if (mapSize === maxMapSize) {
+        btnZoomOut.classList.add("inactive");
+    } else if (mapSize === 3) {
+        btnZoomIn.classList.add("inactive");
+    } else {
+        btnZoomOut.classList.remove("inactive");
+        btnZoomIn.classList.remove("inactive");
+    }
+}
+
+function convertCityToMap(city) { //converts the city into the size of the map
+    const map = [...Array(mapSize)].map(() => Array(mapSize).fill(null)); //creates an empty/null 2 dimensional array
+    const mapCenter = (mapSize + 1) / 2;
+    const cityCenter = (city.length + 1) / 2;
+    const diffCenter = Math.abs(cityCenter - mapCenter);
+    if (cityCenter < mapCenter) {
+        const citySize = city.length;
+        for (let row = 0; row < citySize; row++) {
+            for (let col = 0; col < citySize; col++) {
+                map[row + diffCenter][col + diffCenter] = city[row][col];
+            }
+        }
+    } else if (cityCenter > mapCenter) {
+        for (let row = 0; row < mapSize; row++) {
+            for (let col = 0; col < mapSize; col++) {
+                map[row][col] = city[row + diffCenter][col + diffCenter];
+            }
+        }
+    } else {
+        return city;
+    }
+    return map;
+}
+
+function createBuilding(building) { //receives an building object and turns it into html for a building
     if (building === null) {
         return `<p></p>`;
     } else if (building.cost === 0) {
@@ -169,17 +171,12 @@ function createBuilding(building, index = -1, canBeDragged = false) { //receives
                 walls += wall + "Wall ";
             }
         });
-        if (index === -1) {
-            return `<p class="building ${building.type} ${walls}" draggable="${canBeDragged}">${building.cost}</p>`;
-        } else {
-
-            return `<p class="building ${building.type} ${walls}" data-index="${index}" draggable="${canBeDragged}">${building.cost}</p>`;
-        }
+        return `<p class="building ${building.type} ${walls}">${building.cost}</p>`;
     }
 }
 
 function closeCity() {
-    city.classList.remove("flex");
+    cityMap.classList.remove("flex");
 }
 
 
